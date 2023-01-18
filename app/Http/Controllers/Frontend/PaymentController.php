9208a4839c64efa380a361ec56bc8f431a51f15d
hamed Abdollahnezhad <ab.hamed.it@gmail.com>
2023-01-18 15:58:26 +0330

    first commit

---
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Order;
+use App\Payment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+
+class PaymentController extends Controller
+{
+    public function verify(Request $request, $id){
+      $cart = Session::has('cart') ? Session::get('cart') : null;
+
+      $payment = new Payment($cart->totalPrice);
+      $result = $payment->verifyPayment($request->Authority, $request->Status);
+
+      if($result) {
+
+        $order = Order::findOrFail($id);
+        $order->status = 1;
+        $order->save();
+
+        $newPayment = new Payment($cart->totalPrice);
+        $newPayment->authority = $request->Authority;
+        $newPayment->status = $request->Status;
+        $newPayment->RefID = $result->RefID;
+        $newPayment->order_id = $id;
+        $newPayment->save();
+
+        Session::forget('cart');
+        Session::flash('success', 'پرداخت شما با موفقیت انجام شد');
+        return redirect('/profile');
+
+      }else{
+        Session::flash('warning', 'در پرداخت شما خطایی به وجود آمده است لطفا مجددا تلاش کنید.');
+        return redirect('/cart');
+      }
+    }
+}
---
+++ resources/views/frontend/profile/orders.blade.php
@@ -0,0 +1,61 @@
+@extends('frontend.layout.master')
+
+@section('content')
+    <div class="container">
+        @if(Session::has('success'))
+            <div class="alert alert-success">
+                <div>{{session('success')}}</div>
+            </div>
+        @endif
+
+        <div class="row">
+            <aside id="column-right" class="col-sm-3 hidden-xs">
+                <h3 class="subtitle">حساب کاربری</h3>
+                <div class="list-group">
+                    <ul class="list-item">
+                        <li><a href="#">لیست آدرس ها</a></li>
+                        <li><a href="wishlist.html">لیست علاقه مندی</a></li>
+                        <li><a href="{{route('profile.orders')}}">تاریخچه سفارشات</a></li>
+                        <li><a href="#">دانلود ها</a></li>
+                        <li><a href="#">امتیازات خرید</a></li>
+                        <li><a href="#">بازگشت</a></li>
+                        <li><a href="#">تراکنش ها</a></li>
+                        <li><a href="#">خبرنامه</a></li>
+                        <li><a href="#">پرداخت های تکرار شونده</a></li>
+                    </ul>
+                </div>
+            </aside>
+            <!--Middle Part Start-->
+            <div id="content" class="col-sm-9">
+                <div class="alert alert-success">
+                    <div class="table-responsive">
+                        <table class="table no-margin">
+                            <thead>
+                            <tr>
+                                <th class="text-center">شناسه</th>
+                                <th class="text-center">مقدار</th>
+                                <th class="text-center">وضعیت</th>
+
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($orders as $order)
+                                <tr>
+                                    <td class="text-center"><a href="{{route('profile.orders.lists', ['id'=>$order->id])}}">{{$order->id}}</a></td>
+                                    <td class="text-center">{{$order->amount}}</td>
+                                    @if($order->status == 0)
+                                        <td class="text-center"><span class="label label-danger">پرداخت نشده</span></td>
+                                    @else
+                                        <td class="text-center"><span class="label label-success">پرداخت شده</span></td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!--Middle Part End -->
+        </div>
+    </div>
+@endsection
